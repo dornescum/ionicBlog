a214019f9610e61e91c3389d2f59d79aeb27b4fd
dornescu mihai <dornescu@gmail.com>
2024-03-09 12:32:41 +0200

    Remove environments folder from repository

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: '1a976adbecmsh7f1d6379af44f31p1ed895jsn9f2e5c49b3dc',
-  apiUrl: 'https://open-weather13.p.rapidapi.com/city/',
-  apiHost: 'open-weather13.p.rapidapi.com',
-};
